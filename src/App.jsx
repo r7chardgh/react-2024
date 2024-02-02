404791c72b3393dd3561fa255322d8d0414f6aad
richard tsang <richard.tsang9641@gmail.com>
2024-02-02 18:45:20 +0800

    added tic tac toe tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,50 @@
 import React from "react";
 import MyButton from "./components/MyButton";
-import './styles/app.css'
+import "./styles/app.css";
 import Avatar from "./components/Avatar";
 import LoginButton from "./components/LoginButton";
 import ProductList from "./components/ProductList";
 import Parent from "./components/Parent";
+import Row from "./components/tic-tac-toe/Row";
+import Square from "./components/tic-tac-toe/Square";
 const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  name: "Hedy Lamarr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
   imageSize: 90,
 };
 const App = () => {
+  const [squares, setSquares] = React.useState(Array(9).fill(null));
+  const handleClick = (i) => {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  };
   return (
     <>
       <h2>My App</h2>
       <MyButton />
-      <Avatar user={user}/>
-      <LoginButton/>
-      <ProductList/>
-      <Parent/>
+      <Avatar user={user} />
+      <LoginButton />
+      <ProductList />
+      <Parent />
+      <div className="container">
+        <h1>Tic Tac Toe</h1>
+        <Row>
+          <Square value={squares[0]} onSquareClick={()=>handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={()=>handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={()=>handleClick(2)} />
+        </Row>
+        <Row>
+          <Square value={squares[3]} onSquareClick={()=>handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={()=>handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={()=>handleClick(5)} />
+        </Row>
+        <Row>
+          <Square value={squares[6]} onSquareClick={()=>handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={()=>handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={()=>handleClick(8)} />
+        </Row>
+      </div>
     </>
   );
 };
---
+++ src/components/tic-tac-toe/Row.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Row = ({children,...props}) => {
+  return (
+    <div className='board-row' {...props} >{children}</div>
+  )
+}
+
+export default Row
